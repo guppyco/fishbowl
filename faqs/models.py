ca575923c2495548fcf2a497502ae88f84588bf3
AuTN <autk08@gmail.com>
2021-08-28 20:42:26 +0700

    Update model and test

--- faqs/models.py
+++ faqs/models.py
@@ -6,7 +6,12 @@ from django.db import models
 class FAQ(models.Model):
     question = models.CharField(max_length=140)
     answer = RichTextField()
-    order = models.PositiveSmallIntegerField(default=0, blank=False, null=False)
+    order = models.PositiveSmallIntegerField(
+        unique=True,
+        default=0,
+        blank=False,
+        null=False,
+    )
 
     class Meta:
         ordering = ["order", "pk"]
--- faqs/tests.py
+++ faqs/tests.py
@@ -7,15 +7,13 @@ from .models import FAQ
 
 
 class FAQTests(TestCase):
-    def setUp(self):
-        FAQFactory.create_batch(10)
-
     def test_faq_view(self):
+        FAQFactory.create_batch(2)
         url = reverse("faqs:faqs-list")
         response = self.client.get(url)
         self.assertEqual(response.status_code, 200)
         faqs = FAQ.objects.all()
-        self.assertEqual(faqs.count(), 10)
+        self.assertEqual(faqs.count(), 2)
         for faq in faqs:
             self.assertContains(response, faq.question)
             self.assertContains(response, faq.answer)
