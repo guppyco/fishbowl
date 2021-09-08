0953218129f8d136592a715901dfaa46b531be5f
AuTN <autk08@gmail.com>
2021-09-08 16:49:15 +0700

    Add Sentry for Staging

--- accounts/context_processors.py
+++ accounts/context_processors.py
@@ -1,7 +1,13 @@
+import os
+
 from django.conf import settings
 
 
 def extra_context(request):
-    return {
+    context = {
         "base_url": settings.BASE_URL,
     }
+    if os.environ.get("DJANGO_SETTINGS_MODULE") != "settings.local":
+        context["sentry_dsn"] = os.environ.get("SENTRY_DSN")
+
+    return context
--- settings/staging.py
+++ settings/staging.py
@@ -1,6 +1,18 @@
+import sentry_sdk
+from sentry_sdk.integrations.celery import CeleryIntegration
+from sentry_sdk.integrations.django import DjangoIntegration
+
 from .base import *
 
 DEBUG = False
+
+# Sentry Configuration
+sentry_sdk.init(
+    dsn=os.environ.get("SENTRY_DSN"),
+    integrations=[DjangoIntegration(), CeleryIntegration()],
+    environment="staging",
+    send_default_pii=True,
+)
 
 ALLOWED_HOSTS = ["staging.guppy.co"]
 BASE_URL = "https://staging.guppy.co"
