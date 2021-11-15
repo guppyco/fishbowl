0cbf9d8c6c6b6696fa8a56405ce0f8e173df28ad
AuTN <autk08@gmail.com>
2021-11-15 18:33:42 +0700

    Update guppy on mobile and make bubles slower

--- frontend/js/swimming-fish.js
+++ frontend/js/swimming-fish.js
@@ -4,7 +4,7 @@ const bubbleField = document.getElementById('bubble-field');
 //generate bubbles with randomly timed animation durations
 for (i = 0; i < bubbleCount; i++) {
   const randNum = Math.floor(Math.random() * 20) + 1;
-  const animDur = 2 + (0.5 * randNum);
+  const animDur = 3 + (randNum);
   moveEl = document.createElement('div');
   moveEl.setAttribute('class', 'bubble-rise');
   moveEl.setAttribute('style', 'animation-duration: ' + animDur + 's;');
