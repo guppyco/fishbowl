4db075e5311d61c4112d473aaf61eddf9aae756e
AuTN <autk08@gmail.com>
2021-11-17 11:24:06 +0700

    Optimize tests

--- accounts/tests.py
+++ accounts/tests.py
@@ -5,7 +5,7 @@ from freezegun import freeze_time
 from rest_framework.test import APITestCase
 
 from django.contrib.auth import authenticate
-from django.test import RequestFactory, TestCase
+from django.test import TestCase
 from django.urls import reverse
 from django.utils import timezone
 
@@ -78,8 +78,8 @@ class SignupPageTests(TestCase):
         redirect = response.redirect_chain.pop()
         self.assertIn("/signup/success/", redirect[0])
         self.assertEqual(redirect[1], 302)
-        member = UserProfile.objects.latest("created")
-        self.assertEqual(member.get_short_name(), "Name")
+        account = UserProfile.objects.latest("created")
+        self.assertEqual(account.get_short_name(), "Name")
         self.assertContains(
             response, "Google analytics client-side tracking script"
         )
@@ -133,8 +133,8 @@ class SignupPageTests(TestCase):
         Test that the user can login from the login page.
         TODO: Remove the 'login' flag.
         """
-        member = UserProfile.objects.create_user("anna@guppy.co", "test")
-        user = authenticate(username=member.email, password="test")
+        account = UserProfile.objects.create_user("anna@guppy.co", "test")
+        user = authenticate(username=account.email, password="test")
         self.assertIsNotNone(user)
         data = {
             "login-username": "anna@guppy.co",
@@ -151,8 +151,8 @@ class SignupPageTests(TestCase):
         Test that the user can login from the login page.
         then redirect to "next"
         """
-        member = UserProfile.objects.create_user("anna@guppy.co", "test")
-        user = authenticate(username=member.email, password="test")
+        account = UserProfile.objects.create_user("anna@guppy.co", "test")
+        user = authenticate(username=account.email, password="test")
         self.assertIsNotNone(user)
         data = {
             "login-username": "anna@guppy.co",
@@ -209,8 +209,7 @@ class SignupPageTests(TestCase):
 
 class ProfileViewTests(TestCase):
     def setUp(self):
-        self.member = setup_tests(self.client)
-        self.factory = RequestFactory()
+        setup_tests(self.client)
 
     def test_profile_page(self):
         url = reverse("user_profile")
@@ -220,8 +219,7 @@ class ProfileViewTests(TestCase):
 
 class ProfileUpdateTests(TestCase):
     def setUp(self):
-        self.member = setup_tests(self.client)
-        self.factory = RequestFactory()
+        setup_tests(self.client)
 
     def test_profile_page(self):
         url = reverse("user_profile_edit")
